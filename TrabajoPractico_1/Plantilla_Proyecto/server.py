be500ce20de23dac1061f2caf0577d3d83f50aa9
Agustin Grioni <agugrio17@gmail.com>
2025-03-13 12:23:00 -0300

    Cambios de practica html

--- TrabajoPractico_1/Plantilla_Proyecto/server.py
+++ TrabajoPractico_1/Plantilla_Proyecto/server.py
@@ -5,9 +5,7 @@ from modules.config import app
 # Página de inicio
 @app.route('/')
 def index():
-    cantidad = 32
-    return render_template('inicio.html', una_cantidad = cantidad)
+    return render_template('inicio.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
-    
