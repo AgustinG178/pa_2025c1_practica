6dd11436aa82cd69865d56cd90ef3d3967dae210
Agustin Grioni <agugrio17@gmail.com>
2025-03-14 18:14:44 -0300

    .

--- TrabajoPractico_1/proyecto_1/server.py
+++ TrabajoPractico_1/proyecto_1/server.py
@@ -3,8 +3,11 @@ from modules.config import app
 
 @app.route('/')
 def index():
-    cantidad = 32
-    return render_template('inicio.html', una_cantidad = cantidad)
+    return render_template('inicio.html')
+
+"""@app.route("/Juego")
+def index():
+    return render_template("Pagina1.html")"""
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
