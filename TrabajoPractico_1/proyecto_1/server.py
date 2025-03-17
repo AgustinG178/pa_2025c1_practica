40e0f9d093787ab39ffc3f931d213dabc72a2b29
Agustin Grioni <agugrio17@gmail.com>
2025-03-17 16:46:18 -0300

    raw

--- TrabajoPractico_1/proyecto_1/server.py
+++ TrabajoPractico_1/proyecto_1/server.py
@@ -3,20 +3,20 @@ from modules.config import app
 import sys
 import os
 
-"""ruta_juego = "C:\\\\Users\\\\agugr\\\\OneDrive\\\\Programacion\\\\Programacion_Avanzada\\\\REPO_tp1\\\\pa_2025c1_practica\\\\TrabajoPractico_1\\\\proyecto_1\\\\templates\\\\Juego.py"
+ruta_juego = "C:\Users\agugr\OneDrive\Programacion\Programacion_Avanzada\REPO_tp1\pa_2025c1_practica\TrabajoPractico_1\proyecto_1\templates"
 
 sys.path.append(ruta_juego)
 
-from Juego import frases"""
+from Juego import frases
 
 @app.route('/')
 def index():
-    return render_template('inicio.html')   
+    return render_template('inicio.html')
 
 @app.route("/Film Trivia Facil")
 def pagjuego_facil():
     frasespelis = frases
-    return render_template("Facil.html", frasespelis = frases)
+    return render_template("Facil.html", frasespelis=frases)
 
 @app.route("/Film Trivia Normal")
 def pagjuego_normal():
@@ -27,5 +27,4 @@ def pagjuego_dificil():
     return render_template("Dificil.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
-    
+   app.run(host="0.0.0.0", debug=True)
