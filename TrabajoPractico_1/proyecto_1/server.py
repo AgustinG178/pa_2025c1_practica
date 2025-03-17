1ffea47506180b0c2bfd233bcbdad627814b7ee3
Agustin Grioni <agugrio17@gmail.com>
2025-03-17 12:07:37 -0300

    Botones + Paginas

--- TrabajoPractico_1/proyecto_1/server.py
+++ TrabajoPractico_1/proyecto_1/server.py
@@ -3,11 +3,19 @@ from modules.config import app
 
 @app.route('/')
 def index():
-    return render_template('inicio.html')
+    return render_template('inicio.html')   
 
-"""@app.route("/Juego")
-def index():
-    return render_template("Pagina1.html")"""
+@app.route("/Film Trivia Facil")
+def pagjuego_facil():
+    return render_template("Facil.html")
+
+@app.route("/Film Trivia Normal")
+def pagjuego_normal():
+    return render_template("Normal.html")
+
+@app.route("/Film Trivia Dificil")
+def pagjuego_dificil():
+    return render_template("Dificil.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
