983cd0f7653ce276e0a3902c679b3b7e86dd6c1f
Agustin Grioni <agugrio17@gmail.com>
2025-03-19 13:31:25 -0300

    .

--- TrabajoPractico_1/proyecto_1/modules/Juego.py
+++ TrabajoPractico_1/proyecto_1/modules/Juego.py
@@ -1,4 +1,7 @@
 from modules.modulo1 import leer_frases_de_peliculas, seleccionar_frase
+import random
+from flask import render_template, app
+
 
 frases_usadas = []
 
@@ -6,9 +9,23 @@ nombre_archivo = "frases_de_peliculas.txt"
 
 frases = leer_frases_de_peliculas(nombre_archivo)
 
-frase, pelicula = seleccionar_frase(frases) #podriamos meter esta funcion en un for de 3 ciclos cada vez que se seleccione o una dificultad o una respuesta 
+frase, pelicula, frases_usadas = seleccionar_frase(frases) #podriamos meter esta funcion en un for de 3 ciclos cada vez que se seleccione o una dificultad o una respuesta 
                                             #asi se seleccionan 3 películas constantemente
+                                            
+"""dificil"""
 
-print(f"Frase seleccionada: {frase}")
+frases = leer_frases_de_peliculas(nombre_archivo)
+opciones=[]
+frase_random = random.choice(frases)[1] 
 
-print(f"Película: {pelicula}")
+def dif_dificil():
+
+    for i in range(3): 
+        try:
+            opciones.append([seleccionar_frase(frases)[0],seleccionar_frase(frases)[1]]) 
+        except TypeError:
+            return render_template("Inicio.html")
+        
+    
+
+
--- TrabajoPractico_1/proyecto_1/server.py
+++ TrabajoPractico_1/proyecto_1/server.py
@@ -1,8 +1,6 @@
 from flask import render_template, request
 from modules.config import app
-from modules.modulo1 import leer_frases_de_peliculas, seleccionar_frase
-import random
-nombre_archivo = "frases_de_peliculas.txt"
+from modules.Juego import dif_dificil, frase_random, opciones, frases
 
 @app.route('/')
 def index():
@@ -10,12 +8,11 @@ def index():
 
 @app.route("/Film Trivia Facil", methods=["GET", "POST"])
 def pagjuego_facil():
-    frases = leer_frases_de_peliculas(nombre_archivo)
     if request.method == "POST":
         respuesta_usuario = request.form.get("respuesta")
         resultado = procesar_respuesta(respuesta_usuario)
         return render_template("Facil.html", resultado=resultado)
-    return render_template("Facil.html", frases = frases )
+    return render_template("Facil.html", frasesitas = frases )
 
 def procesar_respuesta(respuesta):
     return "Respuesta correcta" if respuesta == "la correcta" else "Respuesta incorrecta"
@@ -25,18 +22,8 @@ def pagjuego_normal():
     return render_template("Normal.html")
 
 @app.route("/Film Trivia Dificil")
-def pagjuego_dificil():
-    frases = leer_frases_de_peliculas(nombre_archivo)
-    opciones=[]
-
-    for i in range(3): 
-        try:
-            opciones.append([seleccionar_frase(frases)[0],seleccionar_frase(frases)[1]]) 
-        except TypeError:
-            return render_template("Inicio.html")     
-
-        frase_random = random.choice(opciones)[1] 
-
+def dificil():    
+    dif_dificil()
     return render_template("Dificil.html",opciones=opciones,frase_random=frase_random)
 
 if __name__ == "__main__":
