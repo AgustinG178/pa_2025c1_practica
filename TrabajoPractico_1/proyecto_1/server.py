f84b47f50ca65098e768d39881b855495e58cf2d
Agustin Grioni <agugrio17@gmail.com>
2025-03-24 16:31:55 -0300

    .

--- TrabajoPractico_1/proyecto_1/server.py
+++ TrabajoPractico_1/proyecto_1/server.py
@@ -6,28 +6,16 @@ from modules.modulos import procesar_respuesta, seleccionar_peli, frases
 def index():    
     return render_template('inicio.html')
 
-@app.route("/Film Trivia Facil", methods=["GET", "POST"])
-def pagjuego_facil():
-    if request.method == "POST":
-        respuesta_usuario = request.form.get("respuesta")
-        resultado = procesar_respuesta(respuesta_usuario)
-        return render_template("Facil.html", resultado=resultado)
-    return render_template("Facil.html", frasesitas = frases )
-
-@app.route("/Film Trivia Normal")
-def pagjuego_normal():
-    return render_template("Normal.html")
-
-@app.route("/Film Trivia Dificil", methods=["GET", "POST"])
+@app.route("/Film Trivia Juego", methods=["GET", "POST"])
 def dificil():
 
     frase, peli_c, peli_i = seleccionar_peli(frases, 10)
 
     if request.method == "POST":
         
-        return render_template("Dificil.html", frase=frase, peli_c=peli_c, peli_i=peli_i)
+        return render_template("juego.html", frase=frase, peli_c=peli_c, peli_i=peli_i)
     
-    return render_template("Dificil.html", frase=frase, peli_c=peli_c, peli_i=peli_i)
+    return render_template("juego.html", frase=frase, peli_c=peli_c, peli_i=peli_i)
 
 if __name__ == "__main__":
    app.run(host="0.0.0.0", debug=True)
