bcaa498435ed9d835c87a2f6d4a945ad17b4c9eb
NicolasRamirez723 <nicoramirez723@gmail.com>
2025-03-27 13:16:38 -0300

    Opcion ronda juego

--- TrabajoPractico_1/proyecto_1/modules/modulos.py
+++ TrabajoPractico_1/proyecto_1/modules/modulos.py
@@ -14,23 +14,43 @@ def leer_frases_de_peliculas(nombre_archivo):
 
 frases = leer_frases_de_peliculas(nombre_archivo)      
 
-def seleccionar_peli(frases, intentos):
-    frases_usadas = []
-    peli_i = []
-    peli_c = []
-    frase = []
+def juego_opciones(frases, intentos):
+    frases_0 = frases
+    opcion_correcta = []
+    pelis=[]
+    juego = [] #[[[opcion_correcta],[peliculas]]] opcion correcta es una lista, en el indice 0 se ubica la frase correcta y en el 1 la pelicula que le corresponde
+               #peliculas es una lista que incluirá 3 peliculas, una justamentes es la que le corresponde a la frase.
+    ronda =[] 
     for _ in range (intentos):
-        sublista_random = random.choice(frases)
-        if sublista_random not in frases_usadas:
-            frase.append(sublista_random[0])
-            peli_c.append(sublista_random[1])
-            for _ in range(2):
-                peli_i.append(random.choice(frases[1]))
-                frases_usadas.append(frase)
-        else:
-            return seleccionar_peli
+        sublista_random = random.choice(frases_0)
+        opcion_correcta.append([sublista_random[0],sublista_random[1]])
+        ronda.append(opcion_correcta)
+        pelis.append(sublista_random[1]) #añadimos la opcion correcta
+        frases_0.remove(sublista_random)
+        
+        for _ in range(2):
 
-    return frase, peli_c, peli_i
+              peli_aleatoria = random.choice(frases_0)
+              pelis.append(peli_aleatoria[1])
+              frases_0.remove(peli_aleatoria)
+        
+        
+        random.shuffle(pelis)
+        ronda.append(pelis) #ronda interpretacion: ronda[0] = opcion correcta, ronda[1] = lista de películas a usar en la ronda
+        juego.append(ronda)
+        #juego[i] = ronda, entonces juego[i][0] = opcion correcta (lista: frase,peli) y juego[i][1] =  peliculas a mostrar
+
+               
+    #devolvemos la opcion correcta junto con una lista juego, que será lo que se mostrará
+    #cada ronda
     
-def procesar_respuesta(respuesta):
-    return "Respuesta correcta" if respuesta == "la correcta" else "Respuesta incorrecta"
+    return juego
+
+if __name__ == "__main__":
+        
+    for ronda in juego_opciones(frases,1):
+        print(f"Frase: {ronda[0][0][0]}")
+        print(f"Opciones: {ronda[1]}")
+
+
+
--- TrabajoPractico_1/proyecto_1/server.py
+++ TrabajoPractico_1/proyecto_1/server.py
@@ -1,6 +1,6 @@
 from flask import render_template, request
 from modules.config import app
-from modules.modulos import procesar_respuesta, seleccionar_peli, frases
+from modules.modulos import procesar_respuesta, juego_opciones, frases
 
 @app.route('/')
 def index():    
@@ -20,14 +20,8 @@ def pagjuego_normal():
 
 @app.route("/Film Trivia Dificil", methods=["GET", "POST"])
 def dificil():
-
-    frase, peli_c, peli_i = seleccionar_peli(frases, 10)
-
-    if request.method == "POST":
-        
-        return render_template("Dificil.html", frase=frase, peli_c=peli_c, peli_i=peli_i)
+     return render_template("Dificil.html")
     
-    return render_template("Dificil.html", frase=frase, peli_c=peli_c, peli_i=peli_i)
 
 if __name__ == "__main__":
    app.run(host="0.0.0.0", debug=True)
