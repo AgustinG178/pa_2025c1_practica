a35b3e8ecb5810e2dae091889df1b87e23b31b88
NicolasRamirez723 <nicoramirez723@gmail.com>
2025-04-03 14:19:42 -0300

    Juego terminado, funciona, faltan detalles

--- TrabajoPractico_1/proyecto_1/modules/modulos.py
+++ TrabajoPractico_1/proyecto_1/modules/modulos.py
@@ -15,32 +15,36 @@ def leer_frases_de_peliculas(nombre_archivo):
 frases = leer_frases_de_peliculas(nombre_archivo)      
 
 def juego_opciones(frases, intentos):
-    frases_0 = frases
-    opcion_correcta = []
-    pelis=[]
+    frases = frases.copy() #copiamos la lista original, de esta manera cada vez que se llame a la funcion juego_opciones, siempre se podrá jugar
+                           #frases.copy() crea una nueva lista con dirección de memoria diferente 
+    
     juego = [] #[[[opcion_correcta],[peliculas]]] opcion correcta es una lista, en el indice 0 se ubica la frase correcta y en el 1 la pelicula que le corresponde
                #peliculas es una lista que incluirá 3 peliculas, una justamentes es la que le corresponde a la frase.
-    ronda =[] 
+    
     for _ in range (intentos):
-        sublista_random = random.choice(frases_0)
+        opcion_correcta = []
+        pelis=[]
+        ronda =[] 
+        sublista_random = random.choice(frases)
         opcion_correcta.append([sublista_random[0],sublista_random[1]])
         ronda.append(opcion_correcta)
         pelis.append(sublista_random[1]) #añadimos la opcion correcta
-        frases_0.remove(sublista_random)
+        frases.remove(sublista_random)
         
         for _ in range(2):
 
-              peli_aleatoria = random.choice(frases_0)
+              peli_aleatoria = random.choice(frases)
               pelis.append(peli_aleatoria[1])
-              frases_0.remove(peli_aleatoria)
+              frases.remove(peli_aleatoria)
         
         
         random.shuffle(pelis)
         ronda.append(pelis) #ronda interpretacion: ronda[0] = opcion correcta, ronda[1] = lista de películas a usar en la ronda
         juego.append(ronda)
+        
         #juego[i] = ronda, entonces juego[i][0] = opcion correcta (lista: frase,peli) y juego[i][1] =  peliculas a mostrar
 
-        return juego
+    return juego
     #devolvemos la opcion correcta junto con una lista juego, que será lo que se mostrará
     #cada ronda
 
@@ -49,12 +53,29 @@ def opcion_correcta(pelicula,pelicula_correcta):
         return True
     else:
         return False
+   
+def escribir_resultados_archivo(usuario,resultado,fecha): #Escribimos en un txt los resultados de un usuario
+    with open("TrabajoPractico_1/proyecto_1/data/resultados.txt", 'a', encoding="utf-8") as historial:
+        historial.write(f"Usuario:{usuario} / Aciertos:{resultado} / Fecha: {fecha}\n")
+
+
+def leer_archivo_resultados ():
+    with open("TrabajoPractico_1/proyecto_1/data/resultados.txt", 'r', encoding="utf-8") as historial:
+        juego_data = historial.read().splitlines()
+        return juego_data
+
 
 if __name__ == "__main__":
-        
-    for ronda in juego_opciones(frases,1):
-        print(f"Frase: {ronda[0][0][0]}")
-        print(f"Opciones: {ronda[1]}")
-
-
+     print(leer_archivo_resultados())
+     for i in leer_archivo_resultados():
+         print(i)
+     """   
+    for ronda_0 in juego_opciones(frases,4):
+        pass
+        print(f"Frase: {ronda_0[0][0][0]}")
+        print(f"Opciones: {ronda_0[1]}")
+    print(len(frases))
+    print(juego_opciones(frases,4))
+    """
+  
 
--- TrabajoPractico_1/proyecto_1/server.py
+++ TrabajoPractico_1/proyecto_1/server.py
@@ -1,37 +1,60 @@
-from flask import render_template, request, session
+from flask import render_template, request, session,redirect,url_for
 from modules.config import app
-from modules.modulos import juego_opciones, frases, opcion_correcta
+from modules.modulos import juego_opciones, frases, opcion_correcta, escribir_resultados_archivo, leer_archivo_resultados
 from datetime import datetime
 
-@app.route('/')
+@app.route("/",methods=["GET","POST"])
 def index():   
-    session.pop("num_aciertos",None) 
-    return render_template('inicio.html')
+    if request.method == "POST":
+    
+        session["usuario"] = request.form.get("input_usuario")
+        session["intentos"] = int(request.form.get("input_intentos"))
+
+        return redirect(url_for("juego"))
+    
+    else:
+        session.pop("num_aciertos",None)
+        session.pop("input_usuario",None)
+        session.pop("input_intentos",None)
+
+        return render_template('inicio.html')
 
 @app.route("/Film Trivia Juego", methods=["GET", "POST"])
-def dificil():
+def juego():
     if "num_aciertos" not in session:
         session["num_aciertos"] = 0
+    intentos = session["intentos"]
+    juego = juego_opciones(frases, intentos)
     
-    try: 
-        juego = juego_opciones(frases, 10)
-        if request.method == "POST":
-
-            peli_correcta = request.form["peli_correcta"]
-            peli_elegida = request.form["respuesta"]
-            
-            if opcion_correcta(peli_correcta,peli_elegida) == True: 
-                  session["num_aciertos"] +=1
+    
+    if request.method == "POST":
                 
-            return render_template("juego.html", juego=juego, resultado = request.method == "POST", aciertos = session["num_aciertos"])
-            #session mantiene el estado de la variable aciertos entre solicitudes, es decir, cuando se llama nuevamente a def dificil,
-            #se mantiene la cantidad de aciertos
+                ronda_actual = int(request.form.get("ronda_actual",0)) + 1 #actualiza el valor de la ronda
 
-        
-        return render_template("juego.html", juego=juego,aciertos = 0) #devulve la primera pagina
-    except IndexError:
-        return render_template('inicio.html',frases=frases)
-
+                if ronda_actual < len(juego) + 1:
+                    peli_correcta = request.form["peli_correcta"]
+                    peli_elegida = request.form["respuesta"]
+                    
+                    if opcion_correcta(peli_correcta,peli_elegida) == True: 
+                        session["num_aciertos"] +=1
+                        
+                    return render_template("juego.html", ronda = juego[ronda_actual-1],num_ronda = ronda_actual, resultado = request.method == "POST", aciertos = session["num_aciertos"])
+                    #session mantiene el estado de la variable aciertos entre solicitudes, es decir, cuando se llama nuevamente a def dificil,
+                    #se mantiene la cantidad de aciertos
+                
+                else:
+                    escribir_resultados_archivo(session["usuario"],session["num_aciertos"],datetime.today())
+                    return render_template("inicio.html")
+                     
+    else:
+        ronda_actual = 1
     
+        return render_template("juego.html", ronda=juego[ronda_actual-1],num_ronda = ronda_actual,aciertos = session["num_aciertos"]) #devulve la primera pagina
+    
+@app.route("/historial", methods=["GET", "POST"])
+def historial():
+     juegos_data = leer_archivo_resultados()
+     return render_template("resultados.html",juegos_data = juegos_data)
+     
 if __name__ == "__main__":
    app.run(host="0.0.0.0", debug=True)
