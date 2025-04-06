50fed3c2dba8b1a1cd696f736e5ab16ddbc2131c
Agustin Grioni <agugrio17@gmail.com>
2025-04-06 20:34:30 -0300

    Co-authored-by: NicolasRamirez723 <NicolasRamirez723@users.noreply.github.com>

--- TrabajoPractico_1/proyecto_1/modules/modulos.py
+++ TrabajoPractico_1/proyecto_1/modules/modulos.py
@@ -5,7 +5,7 @@ nombre_archivo = "frases_de_peliculas.txt"
 def leer_frases_de_peliculas(nombre_archivo):
 
     frases = []
-    with open("TrabajoPractico_1/proyecto_1/data/frases_de_peliculas.txt", 'r', encoding="utf-8") as archivo:
+    with open("data/frases_de_peliculas.txt", 'r', encoding="utf-8") as archivo:
         for linea in archivo:
             linea = linea.strip()
             if ';' in linea:  
@@ -14,28 +14,30 @@ def leer_frases_de_peliculas(nombre_archivo):
 
 frases = leer_frases_de_peliculas(nombre_archivo)      
 
-def juego_opciones(frases, intentos):
-    frases = frases.copy() #copiamos la lista original, de esta manera cada vez que se llame a la funcion juego_opciones, siempre se podrá jugar
-                           #frases.copy() crea una nueva lista con dirección de memoria diferente 
+lista_frases = frases.copy()
+
+def juego_opciones(lista_frases, intentos):
+    #copiamos la lista original, de esta manera cada vez que se llame a la funcion juego_opciones, siempre se podrá jugar
+    #lista_frases.copy() crea una nueva lista con dirección de memoria diferente 
     
     juego = [] #[[[opcion_correcta],[peliculas]]] opcion correcta es una lista, en el indice 0 se ubica la frase correcta y en el 1 la pelicula que le corresponde
-               #peliculas es una lista que incluirá 3 peliculas, una justamentes es la que le corresponde a la frase.
+    #peliculas es una lista que incluirá 3 peliculas, una justamentes es la que le corresponde a la frase.
     
     for _ in range (intentos):
         opcion_correcta = []
         pelis=[]
         ronda =[] 
-        sublista_random = random.choice(frases)
+        sublista_random = random.choice(lista_frases)
         opcion_correcta.append([sublista_random[0],sublista_random[1]])
         ronda.append(opcion_correcta)
         pelis.append(sublista_random[1]) #añadimos la opcion correcta
-        frases.remove(sublista_random)
+        lista_frases.remove(sublista_random)
         
         for _ in range(2):
 
-              peli_aleatoria = random.choice(frases)
-              pelis.append(peli_aleatoria[1])
-              frases.remove(peli_aleatoria)
+            peli_aleatoria = random.choice(lista_frases)
+            pelis.append(peli_aleatoria[1])
+            lista_frases.remove(peli_aleatoria)
         
         
         random.shuffle(pelis)
@@ -55,12 +57,12 @@ def opcion_correcta(pelicula,pelicula_correcta):
         return 0
    
 def escribir_resultados_archivo(usuario,resultado,intentos,fecha): #Escribimos en un txt los resultados de un usuario
-    with open("TrabajoPractico_1/proyecto_1/data/resultados.txt", 'a', encoding="utf-8") as historial:
+    with open("data/resultados.txt", 'a', encoding="utf-8") as historial:
         historial.write(f"Usuario:{usuario} / Resultado: {resultado}/{intentos} / Fecha: {fecha}\n")
 
 
 def leer_archivo_resultados ():
-    with open("TrabajoPractico_1/proyecto_1/data/resultados.txt", 'r', encoding="utf-8") as historial:
+    with open("data/resultados.txt", 'r', encoding="utf-8") as historial:
         juego_data = historial.read().splitlines()
         return juego_data
 
@@ -71,11 +73,11 @@ if __name__ == "__main__":
      for i in leer_archivo_resultados():
          print(i)
 
-     for ronda_0 in juego_opciones(frases,4):
+     for ronda_0 in juego_opciones(lista_frases,4):
         print(f"Frase: {ronda_0[0][0][0]}")
         print(f"Opciones: {ronda_0[1]}")
-        print(len(frases))  
-        print(juego_opciones(frases,4))
+        print(len(lista_frases))  
+        print(juego_opciones(lista_frases,4))
     
   
 
