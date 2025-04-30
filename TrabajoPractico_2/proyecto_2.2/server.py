b168eedc6592d60ea93ed77c8c8c62a08a96651c
Agustin Grioni <agugrio17@gmail.com>
2025-04-30 07:07:49 -0300

    atualizo el server, 100% listo para primer revision asincronica

--- TrabajoPractico_2/proyecto_2.2/server.py
+++ TrabajoPractico_2/proyecto_2.2/server.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
-from modules.verduleria import CintaTransportadora, Cajon, FabricaDeAlimentos, Sensor, AnalizadorDeCajon, GeneradorDeInforme
+from modules.sensor import Sensor, FabricaDeAlimentos
+from modules.cajon import Cajon, AnalizadorDeCajon, GeneradorDeInforme
+from modules.cinta_transportadora import CintaTransportadora
 
 app = Flask(__name__)
 
@@ -10,30 +12,26 @@ def iniciar():
 
     if request.method == "POST":
         try:
-            # Obtener el número total de alimentos deseados desde el formulario
+            
             total_alimentos_deseados = int(request.form["total_alimentos"])
+        
         except (KeyError, ValueError):
+        
             return "Error: Datos no válidos o no proporcionados.", 400
 
-        # Configurar la capacidad del cajón
-        capacidad = total_alimentos_deseados
-
-        # Crear los objetos necesarios
         fabrica = FabricaDeAlimentos()
+        
         sensor = Sensor(fabrica)
-        cajon = Cajon(capacidad)
+        
+        cajon = Cajon(total_alimentos_deseados)
+        
         cinta = CintaTransportadora(sensor, cajon)
 
-        # Iniciar el transporte de alimentos
         cinta.iniciar_transporte()
 
-        # Calcular las métricas del cajón
         metricas = AnalizadorDeCajon.calcular_metricas(cajon)
-
-        # Generar el informe con advertencias
         advertencias = GeneradorDeInforme.generar_advertencias(metricas)
 
-    # Renderizar la plantilla HTML con las métricas y advertencias
     return render_template("inicio.html", metricas=metricas, advertencias=advertencias)
 
 if __name__ == "__main__":
