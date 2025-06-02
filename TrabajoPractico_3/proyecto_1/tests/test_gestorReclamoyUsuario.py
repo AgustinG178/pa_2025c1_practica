f1fbf2119d7fe83d62c1eb2ac5fa63a256d7bb8f
NicolasRamirez723 <nicoramirez723@gmail.com>
2025-06-02 09:49:34 -0300

    test_gestor_reclamo, se prueba el metodo crear reclamo

--- TrabajoPractico_3/proyecto_1/tests/test_gestorReclamoyUsuario.py
+++ TrabajoPractico_3/proyecto_1/tests/test_gestorReclamoyUsuario.py
@@ -4,7 +4,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,session
 from modules.gestor_reclamos import GestorReclamo
 from modules.gestor_usuario import GestorDeUsuarios
-from modules.modelos import Base
+from modules.modelos import Base,Usuario,Reclamo,Departamento
+from modules.usuarios import UsuarioFinal
+from modules.repositorio import RepositorioReclamosSQLAlchemy,RepositorioUsuariosSQLAlchemy
 class TestGestorReclamo(unittest.TestCase):
 
     def setUp(self):
@@ -18,7 +20,15 @@ class TestGestorReclamo(unittest.TestCase):
         self.Session = sessionmaker(bind=self.engine)
 
         self.session = self.Session()
-        
+
+        self.repo_usuarios = RepositorioUsuariosSQLAlchemy(self.session)
+
+        self.repo_reclamos = RepositorioReclamosSQLAlchemy(self.session)
+
+        self.gestor_reclamo = GestorReclamo
+
+        self.gestor_usuario = GestorDeUsuarios
+
     def tearDown(self):
 
         """
@@ -27,3 +37,32 @@ class TestGestorReclamo(unittest.TestCase):
         self.session.close()
 
         self.engine.dispose()
+    
+    
+    def test_crear_reclamo(self):
+        
+        """
+        Se testea el gestor_reclamo para crear reclamos 
+        """
+
+        p_usuario = UsuarioFinal(nombre="Juan", email="juan@gmai.com", contraseña="juan123", apellido="pereira", nombre_de_usuario="juanpe124",claustro="estudiante",rol=None)
+
+        usuario_tabla = p_usuario.map_to_modelo_bd()
+
+        dpto_prueba = Departamento(nombre="matematica",jefe=usuario_tabla.id)
+
+        descripcion = "No hay papel"
+
+        self.gestor_reclamo.crear_reclamo(usuario=usuario_tabla, descripcion=descripcion, departamento=dpto_prueba)
+
+        reclamo_base = self.session.query(Reclamo).filter_by(contenido = descripcion).first()
+
+        self.assertEqual(reclamo_base.contenido,descripcion)
+
+    def test_buscar_reclamos_por_usuario(self):
+
+        """
+        Se testea la capacidad de devolver todos los reclamos relacionados a un usuarios
+        """
+
+        
