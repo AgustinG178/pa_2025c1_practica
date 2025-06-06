2fb7e3f6e23ec76d90a9c5248bb7bcad37a0fcc6
Agustin Grioni <agugrio17@gmail.com>
2025-06-06 10:25:25 -0300

    elimino import

--- TrabajoPractico_3/proyecto_1/server.py
+++ TrabajoPractico_3/proyecto_1/server.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, request, redirect, url_for, session
+from flask import render_template, flash, request, redirect, url_for
 from flask_login import login_required, logout_user, current_user
 from modules.config import app, login_manager, crear_engine   
 from modules.repositorio import RepositorioUsuariosSQLAlchemy, RepositorioReclamosSQLAlchemy
